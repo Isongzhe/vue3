3dd0d101ee7605283e84de86e2332be6af419af8
SUNG-CHE LIN <86179263+Isongzhe@users.noreply.github.com>
2024-05-11 00:18:00 +0800

    update

--- course/嚮應式數據/toRefs解構賦值.vue
+++ course/嚮應式數據/toRefs解構賦值.vue
@@ -1,59 +1,59 @@
 <template>
-  <div class="person">
+<div class="person">
     <h2>姓名: {{user.name}}</h2>
     <h2>年齡: {{user.age}}</h2>
     <h2>年齡(Refs解構後): {{age}}</h2>
 
     <button @click="changeName">修改名字</button>
     <button @click="changeAge">修改年齡</button>
-  
+
     <h2>車子品牌: {{user.car.brand}}</h2>
     <h2>車子價格(萬): {{user.car.price}}</h2>
     <button @click="changePrice">價格+10萬</button>
-  </div>
+</div>
 </template>
 
 <script lang="ts">
 // 暴露組件名稱"Person.vue"讓外部引用
 export default {
-  name: 'Person',
+name: 'Person',
 }
 </script>
 
 <script lang="ts" setup>
-  import { ref,reactive,toRefs,toRef } from 'vue'
-  
+import { ref,reactive,toRefs,toRef } from 'vue'
+
 
 // Data數據
-  let user = reactive({
+let user = reactive({
     name: 'John',
     age: 30,
     car: {
-      brand: 'Toyota',
-      price: 100
+    brand: 'Toyota',
+    price: 100
     },
     games: [
-      {id:'game001', name:'LOL'},
-      {id:'game002', name:'PUBG'},
-      {id:'game003', name:'APEX'},
+    {id:'game001', name:'LOL'},
+    {id:'game002', name:'PUBG'},
+    {id:'game003', name:'APEX'},
     ]
-  })
+})
 
 // 解構賦值，使用toRefs將reactive物件屬性轉換為新的ref物件(但值仍然是reactive的)
-  let {name,age,car,games} = toRefs(user)
-  // let n1 = toRef(user,'name') 跟上面概念一樣，只是轉換單一屬性(少用)
+let {name,age,car,games} = toRefs(user)
+// let n1 = toRef(user,'name') 跟上面概念一樣，只是轉換單一屬性(少用)
 
 // Methods方法
-  const changeName = () => {
+const changeName = () => {
     name.value = 'Tom'
-  }
-  const changeAge = () => {
+}
+const changeAge = () => {
     age.value += 1
-  }
+}
 
-  const changePrice = () => {
+const changePrice = () => {
     car.value.price += 10
-  }
+}
 </script>
 
 <style scoped>
@@ -66,22 +66,22 @@ export default {
     margin: auto;
 
     div{
-      border: 3px solid #ccc;
-      padding: 15px;
+    border: 3px solid #ccc;
+    padding: 15px;
     }
 }
 h2{
-  color: white;
+color: white;
 }
 button{
     margin: 0 5px;
 }
 ul{
-  list-style-type: none;
-  padding: 0;
+list-style-type: none;
+padding: 0;
 }
 li{
-  font-size: 20px;
-  color: white;
+font-size: 20px;
+color: white;
 }
 </style>
