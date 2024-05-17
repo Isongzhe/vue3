8e90a7be07dcc7eb3fc9cf09b60f45d7cea5b567
SUNG-CHE LIN <86179263+Isongzhe@users.noreply.github.com>
2024-05-17 10:20:18 +0800

    完成表單

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,109 +1,230 @@
 <template>
     <div class="container">
-        <label>Google地圖清單連結:
-            <input type="text" name="name" v-model="userData.googleMapURL">
-        </label>
-        <label>是否來回相同機場:
-            <select v-model="sameAirport">
-                <option value="true">是</option>
-                <option value="false">否</option>
-            </select>
-        </label>
-        <label>抵達機場:
-            <select v-model="userData.arrivalAirport">
-                <option v-for="(airport, index) in airports" :key="index" :value="airport">
-                    {{ airport.name }}
-                </option>
-            </select>
-            <span>抵達機場: {{ userData.arrivalAirport }}</span>
-        </label>
-        <label>回程機場:
-            <select v-model="userData.returnAirport">
-                <option v-for="(airport, index) in airports" :key="index" :value="airport">
-                    {{ airport.name }}
-                </option>
-            </select>
-            <span>回程機場: {{ userData.returnAirport }}</span>
-        </label>
-        <button type="submit" @click="handleSubmit()">提交</button>
+        <ul>
+            <li>
+                <p>Google地圖清單連結:</p>
+                <p v-if="formData.googleMapURLError" class="error-message">{{ formData.googleMapURLError }}</p>
+                <input type="text" v-model="formData.googleMapURL" :class="{ 'error': formData.googleMapURLError ,'full-width': true }"
+                    placeholder="https://maps.app.goo.gl/..." />
+            </li>
+            <li>是否來回相同機場:
+                <select v-model="sameAirport">
+                    <option value="true">是</option>
+                    <option value="false">否</option>
+                </select>
+            </li>
+            <li>
+                抵達機場:<select v-model="formData.arrivalAirport">
+                    <option v-for="(airport, index) in airports" :key="index" :value="airport">
+                        {{ airport.name }}
+                    </option>
+                </select><br>
+                <!-- <span>抵達機場: {{ formData.arrivalAirport }}</span> -->
+                回程機場:<select v-model="formData.returnAirport">
+                    <option v-for="(airport, index) in airports" :key="index" :value="airport">
+                        {{ airport.name }}
+                    </option>
+                </select>
+                <!-- <span>回程機場: {{ formData.returnAirport }}</span> -->
+            </li>
+            <li class="dateTimeRange">請輸入旅遊抵達日期與離境日期:
+                <flat-pickr v-model="dateRange" :config="config" /><br>
+                請輸入開始旅行的時間: <input type="time" v-model="timeRange.start" /><br>
+                請輸入結束旅行的時間:<input type="time" v-model="timeRange.end" /><br>
+            </li>
+            <li>
+                <button type="submit" @click="submitForm()">提交</button>
+            </li>
+        </ul>
     </div>
 </template>
 
-<script lang="ts">
+<!-- <script lang="ts">
 export default {
     name: 'Form',
 }
-</script>
+</script> -->
 
 <script lang="ts" setup>
-import { ref, watchEffect } from 'vue'
+import FlatPickr from 'vue-flatpickr-component';
+import 'flatpickr/dist/flatpickr.css';
+import type { BaseOptions } from 'flatpickr/dist/types/options';
+import { ref, watchEffect, computed } from 'vue'
+
+const config: Partial<BaseOptions> = {
+    minDate: "today",
+    mode: 'range',
+    dateFormat: 'Y-m-d',
+};
+
+interface TravelTime {
+    start: string;
+    end: string;
+}
 
 interface Airport {
     name: string;
     coordinates: [number, number];
 }
-interface UserData {
+interface FormData {
     googleMapURL: string;
+    googleMapURLError: string;
     arrivalAirport: Airport;
+    arrivalAirportError: string;
     returnAirport: Airport;
+    returnAirportError: string;
+    dateTimeRange: TravelTime;
+    dateTimeRangeError: string;
+    dateList: string[];
+    dateListError: string;
 }
+
 let sameAirport = ref(false)
+let dateRange = ref('')
+let timeRange = ref<TravelTime>({ start: '', end: '' })
 
 let airports = ref<Airport[]>([
-    { name: '羽田機場(HND)', coordinates: [110, 200] },
-    { name: '成田機場(NRT)', coordinates: [130, 220] },
+    { name: '成田機場(NRT)', coordinates: [35.7641817, 140.3847858] },
+    { name: '羽田機場(HND)', coordinates: [35.5493932, 139.7798386] },
 ])
 
-let userData = ref<UserData>({
+let formData = ref<FormData>({
     googleMapURL: '',
+    googleMapURLError: '',
     arrivalAirport: { name: '', coordinates: [0, 0] },
+    arrivalAirportError: '',
     returnAirport: { name: '', coordinates: [0, 0] },
+    returnAirportError: '',
+    dateTimeRange: { start: '', end: '' },
+    dateTimeRangeError: '',
+    dateList: [],
+    dateListError: '',
 })
 
+let isFormValid = computed(() => {
+    return formData.value.googleMapURL &&
+        formData.value.arrivalAirport.name &&
+        formData.value.returnAirport.name &&
+        formData.value.dateTimeRange.start &&
+        formData.value.dateTimeRange.end &&
+        formData.value.dateList.length;
+});
 
-const handleSubmit = () => {
-    console.log(userData.value.googleMapURL)
-    console.log(userData.value.arrivalAirport)
-    console.log(userData.value.returnAirport)
+// 繳交表單
+function submitForm() {
+    if (!formData.value.googleMapURL) {
+        formData.value.googleMapURLError = '請填寫Google Map清單連結';
+    }
+    if (!formData.value.arrivalAirport.name) {
+        formData.value.arrivalAirportError = '請選擇到達機場';
+    }
+    if (!formData.value.returnAirport.name) {
+        formData.value.returnAirportError = '請選擇返回機場';
+    }
+    if (!formData.value.dateTimeRange.start || !formData.value.dateTimeRange.end) {
+        formData.value.dateTimeRangeError = '請選擇日期和時間範圍';
+    }
+    if (!formData.value.dateList.length) {
+        formData.value.dateListError = '請選擇日期';
+    }
+    if (isFormValid.value) {
+        // 表單提交成功，這裡可以添加你的代碼
+        console.log('表單提交成功');
+    }
 }
 
+// 監聽是否來回相同機場，如果是則回程機場自動填入抵達機場
 watchEffect(() => {
     if (sameAirport.value) {
-        userData.value.returnAirport = userData.value.arrivalAirport
+        formData.value.returnAirport = formData.value.arrivalAirport
     }
 })
 
+// 監聽Google地圖清單連結，如果符合格式則發出請求
+watchEffect(() => {
+    if (formData.value.googleMapURL === '') {
+        // 如果 googleMapURL 為空，則直接返回
+        return;
+    }
+    let urlPattern = /^https:\/\/maps\.app\.goo\.gl\/.+$/
+    if (urlPattern.test(formData.value.googleMapURL)) {
+        // 發出請求
+        // 你需要替換這裡的代碼來發出實際的請求
+        console.log('發出請求: ' + formData.value.googleMapURL);
+    } else {
+        console.log('Google地圖清單連結格式錯誤');
+    }
+})
+
+// 監聽日期區間，將日期與時間合併成一個物件
+watchEffect(() => {
+    let start = dateRange.value.split(' to ')[0]
+    let end = dateRange.value.split(' to ')[1]
+    formData.value.dateTimeRange = {
+        start: `${start}T${timeRange.value.start}`,
+        end: `${end}T${timeRange.value.end}`
+    }
+})
+
+// 生成日期列表，計算出旅遊日期的每一天，存成列表
+let dateList = computed(() => {
+    let dates = []
+    let start = new Date(dateRange.value.split(' to ')[0])
+    let end = new Date(dateRange.value.split(' to ')[1])
+    while (start <= end) {
+        dates.push(new Date(start).toLocaleDateString('zh-TW', { month: '2-digit', day: '2-digit' }))
+        start.setDate(start.getDate() + 1)
+    }
+    formData.value.dateList = dates
+    return dates
+})
 </script>
 
 
 <!-- scoped局部樣式，css樣式只在當前組件內生效 -->
 <!-- 很重要，避免開發時樣式互相干擾 -->
 <style scoped>
-.container {
-    background-color: rgb(0, 183, 255);
+@import 'flatpickr/dist/flatpickr.min.css';
+
+* {
+    border: 1px solid black;
+}
+
+ul {
+    background-color: rgba(0, 0, 0, 0);
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
     padding: 20px;
     border-radius: 10px;
     margin: 0 auto;
-    width: 800px;
-    display: flex;
+    max-width: 100%;
+    box-sizing: border-box;
+
+    display: block;
     flex-wrap: wrap;
     align-items: center;
-    /* 新增這行來使 form 中的元素垂直置中 */
     justify-content: center;
-    /* 新增這行來使 form 中的元素水平置中 */
 }
 
-label {
-    background-color: aquamarine;
+li {
+    background-color: rgb(22, 198, 210);
     display: block;
     text-align: start;
-    margin: 10px 10px;
-    width: 100%;
-
+    padding: 10px 10px;
+    margin: 20px 0;
+    width: 30%;
+}
+li input{
+    width: 100px;
+    margin-top: 10px;
+}
+.full-width {
+    width: 50%;
+}
+.error {
+    border-color: red;
 }
 
-span {
-    margin-left: 10px;
+.error-message {
+    color: red;
 }
 </style>
