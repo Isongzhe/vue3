154bc9bd64c3aac128c79b26ac45f5fc29ebcbb2
SUNG-CHE LIN <86179263+Isongzhe@users.noreply.github.com>
2024-06-19 08:39:02 +0800

    rebuild docs for url

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import VueSetupExtend from "vite-plugin-vue-setup-extend";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/vue3/" : "/",
   plugins: [vue(), VueSetupExtend()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
+  },
+  build: {
+    outDir: "docs", // 將輸出目錄設定為 'docs'
   },
   optimizeDeps: {
     include: ["element-plus"],
